1c26d5d6ccf5d7bc15f0d7ba50a8166cb1b57744
Oliver Sierra <oliver.sierra@ignition.sapient.com>
2018-10-04 15:19:05 -0700

    Sample dynamic route

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,30 @@
 import Navbar from '../components/Navbar';
 import Head from 'next/head';
+import Link from 'next/link';
 
-const Index = () => (
+const Index = (props) => (
   <section>
     <Head>
       <title>Hello NextJS</title>
     </Head>
     <h1>Hello NextJS</h1>
     <Navbar />
+    <div>
+      <Link as={`/posts/${props.slug}`} href={`/post?title=${props.title}`}>
+        <a>Latest Post NextJS</a>
+      </Link>
+    </div>
   </section>
-)
+);
+
+Index.getInitialProps = async function(props) {
+  let title = 'This is the title';
+  let slug = 'this-is-the-title';
+
+  return {
+    title,
+    slug
+  }
+}
 
 export default Index;
--- server/index.js
+++ server/index.js
@@ -15,6 +15,12 @@ app
 
     server.use('/api', extraRoutes);
 
+    server.get('/posts/:slug', (req, res) => {
+      let actualPage = '/post';
+      let queryParam = { title: req.params.slug };
+      app.render(req, res, actualPage, queryParam);
+    });
+
     server.get('*', (req, res) => {
       return handle(req, res);
     });
